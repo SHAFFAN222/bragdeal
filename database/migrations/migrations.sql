5136dbd062266a1889a58cbb3e840d6964ce40a6
Daniyal-web832 <149469104+daniyal268@users.noreply.github.com>
2024-04-26 17:21:00 +0500

    update

--- database/migrations/migrations.sql
+++ database/migrations/migrations.sql
@@ -2,24 +2,14 @@ ALTER TABLE tickets
 ADD COLUMN subject VARCHAR(255),
 ADD COLUMN department VARCHAR(255),
 ADD COLUMN priority VARCHAR(255);
-
 INSERT INTO `roles` (`id`, `name`) VALUES (NULL, 'superadmin');
 INSERT INTO `roles` (`id`, `name`) VALUES (NULL, 'client');
-
-
-
-
 ALTER TABLE `article` ADD `category` VARCHAR(100) NOT NULL AFTER `create_at`;
-
 ALTER TABLE `projects` ADD `description` TEXT NOT NULL AFTER `created_at`;
-
-
 ALTER TABLE `projects` CHANGE `description` `description` 
 TEXT CHARACTER SET 
 utf8mb4 COLLATE utf8mb4_general_ci
  NULL DEFAULT NULL;
-
-
 CREATE TABLE`invoice`( 
   `id` int(11) NOT NULL,
  `client_id` int(11) NOT NULL,
@@ -28,7 +18,6 @@ CREATE TABLE`invoice`(
   `created_at` datetime NOT NULL,
    `update_at` datetime NOT NULL
    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
 --
 INSERT INTO `invoice` (`id`, `client_id`, `project_id`, `price`, `created_at`, `update_at`) VALUES ('1', '4', '5', '5000', current_timestamp(), current_timestamp());
 ALTER TABLE `portal_settings` ADD `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER `created_at`;
